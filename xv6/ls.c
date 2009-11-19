6518ef80929b69fa5534548404044efd5710b2fe
Will McCardell <mccardel@cs.wisc.edu>
2009-11-19 11:45:09 -0600

    Revert "Revert "Fixed ls to output whether a file is a directory or not.""
    This reverts commit 718bee8a8f61d1b9cdc509ceb72f4746ff463b4e.

--- xv6/ls.c
+++ xv6/ls.c
@@ -43,7 +43,8 @@ ls(char *path)
   
   switch(st.type){
   case T_FILE:
-    printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
+    //printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
+    printf(1, "%s %s %d %d\n", fmtname(path), "FILE", st.ino, st.size);
     break;
   
   case T_DIR:
@@ -63,7 +64,15 @@ ls(char *path)
         printf(1, "ls: cannot stat %s\n", buf);
         continue;
       }
-      printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+      char *tmp;
+      if(st.type == T_DIR)
+          tmp = "DIR ";
+      else if (st.type == T_FILE )
+          tmp = "FILE";
+      else if (st.type == T_DEV )
+          tmp = "DEV ";
+
+      printf(1, "%s %s %d %d\n", fmtname(buf), tmp, st.ino, st.size);
     }
     break;
   }
